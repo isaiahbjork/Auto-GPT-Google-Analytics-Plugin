83f23705d4d9b741ebaa9372b0ea450f71333142
isaiah <isaiah98981@icloud.com>
2023-04-22 15:02:43 -0700

    updates

--- README.md
+++ README.md
@@ -1,33 +1,82 @@
-# Auto-GPT-Plugin-Template
-A starting point for developing your own external plug-in for Auto-GPT
+# Auto-GPT Google Analytics Plugin 📊
+The AutoGPT Google Analytics Plugin is a software tool that allows you to connect Google Analytics to Auto-GPT.
 
-# **If you want your plugin to live within the codebase, use the example in the [plugins repo](https://github.com/Significant-Gravitas/Auto-GPT-Plugins) instead**
+[![GitHub Repo stars](https://img.shields.io/github/stars/isaiahbjork/Auto-GPT-Google-Analytics-Plugin?style=social)](https://github.com/isaiahbjork/Auto-GPT-Google-Analytics-Plugin/stargazers)
 
-### Plugin Installation Steps
 
-1. **Clone or download the plugin repository:**
-   Clone the plugin repository, or download the repository as a zip file.
-  
-   ![Download Zip](https://raw.githubusercontent.com/BillSchumacher/Auto-GPT/master/plugin.png)
 
-2. **Install the plugin's dependencies (if any):**
-   Navigate to the plugin's folder in your terminal, and run the following command to install any required dependencies:
+## 💡 Key Features:
+- 📊 **Get Website Analytics**
 
-   ``` shell
-      pip install -r requirements.txt
-   ```
+## 🔧 Installation
 
-3. **Package the plugin as a Zip file:**
-   If you cloned the repository, compress the plugin folder as a Zip file.
+Follow these steps to configure the Auto-GPT Google Analytics Plugin:
 
-4. **Copy the plugin's Zip file:**
-   Place the plugin's Zip file in the `plugins` folder of the Auto-GPT repository.
+### 1. Clone the Auto-GPT-Google-Analytics-Plugin repository
+Clone this repository and navigate to the `Auto-GPT-Google-Analytics-Plugin` folder in your terminal:
 
-5. **Allowlist the plugin (optional):**
-   Add the plugin's class name to the `ALLOWLISTED_PLUGINS` in the `.env` file to avoid being prompted with a warning when loading the plugin:
+```bash
+git clone https://github.com/isaiahbjork/Auto-GPT-Google-Analytics-Plugin.git
+```
 
-   ``` shell
-   ALLOWLISTED_PLUGINS=example-plugin1,example-plugin2,example-plugin3
-   ```
+### 2. Install required dependencies
+Execute the following command to install the necessary dependencies:
 
-   If the plugin is not allowlisted, you will be warned before it's loaded.
+```bash
+pip install -r requirements.txt
+```
+
+### 3. Package the plugin as a Zip file
+Compress the `Auto-GPT-Google-Analytics-Plugin` folder or [download the repository as a zip file](https://github.com/isaiahbjork/Auto-GPT-Google-Analytics-Plugin/archive/refs/heads/master.zip).
+
+### 4. Install Auto-GPT
+If you haven't already, clone the [Auto-GPT](https://github.com/Significant-Gravitas/Auto-GPT) repository, follow its installation instructions, and navigate to the `Auto-GPT` folder.
+
+You might have to run this in the Auto-GPT file if you get an error saying "No Moudle Found".
+
+```bash
+pip install ta
+```
+
+### 5. Copy the Zip file into the Auto-GPT Plugin folder
+Transfer the zip file from step 3 into the `plugins` subfolder within the `Auto-GPT` repo.
+
+### 6. Locate the `.env.template` file
+Find the file named `.env.template` in the main `/Auto-GPT` folder.
+
+### 7. Create and rename a copy of the file
+Duplicate the `.env.template` file and rename the copy to `.env` inside the `/Auto-GPT` folder.
+
+### 8. Edit the `.env` file
+Open the `.env` file in a text editor. Note: Files starting with a dot might be hidden by your operating system.
+
+### 9. Add Google Analytics configuration settings
+Append the following configuration settings to the end of the file:
+
+```ini
+################################################################################
+### GOOGLE ANALYTICS
+################################################################################
+GOOGLE_APPLICATION_CREDENTIALS=
+GOOGLE_ANALYTICS_VIEW_ID=
+```
+- Create a Google service account.
+- Add the email address to your Google Analytics (Admin -> View Access Management).
+- Add service account json file to Auto-GPT.
+
+### 10. Allowlist Plugin
+In your `.env` search for `ALLOWLISTED_PLUGINS` and add this Plugin:
+
+```ini
+################################################################################
+### ALLOWLISTED PLUGINS
+################################################################################
+#ALLOWLISTED_PLUGINS - Sets the listed plugins that are allowed (Example: plugin1,plugin2,plugin3)
+ALLOWLISTED_PLUGINS=AutoGoogleAnalyticsPlugin
+```
+
+## 🧪 Test the Auto-GPT Google Analytics Plugin
+
+Experience the plugin's capabilities by getting your website analytics.
+
+
--- src/auto_gpt_google_analytics/__init__.py
+++ src/auto_gpt_google_analytics/__init__.py
@@ -1,10 +1,17 @@
-"""This is a template for Auto-GPT plugins."""
-import abc
-from typing import Any, Dict, List, Optional, Tuple, TypeVar, TypedDict
+"""This is a plugin to use Auto-GPT with MetaTrader."""
+from typing import Any, Dict, List, Optional, Tuple, TypeVar, TypedDict, Union
+from auto_gpt_plugin_template import AutoGPTPluginTemplate
 
-from abstract_singleton import AbstractSingleton, Singleton
+# Google Analytics
+import os
+from gaapi4py import GAClient
+
+c = GAClient()
 
 PromptGenerator = TypeVar("PromptGenerator")
+
+os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+view_id = os.environ('GOOGLE_ANALYTICS_VIEW_ID')
 
 
 class Message(TypedDict):
@@ -12,234 +19,238 @@ class Message(TypedDict):
     content: str
 
 
-class AutoGPTPluginTemplate(AbstractSingleton, metaclass=Singleton):
+class AutoGPTGoogleAnalyticsPlugin(AutoGPTPluginTemplate):
     """
-    This is a template for Auto-GPT plugins.
+    This is a plugin to use Auto-GPT with Google Analytics.
     """
 
     def __init__(self):
         super().__init__()
-        self._name = "Auto-GPT-Plugin-Template"
+        self._name = "Auto-GPT-Google-Analytics"
         self._version = "0.1.0"
-        self._description = "This is a template for Auto-GPT plugins."
+        self._description = "This is a plugin for Auto-GPT-Google-Analytics."
 
-    @abc.abstractmethod
+    def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
+        prompt.add_command(
+            "Get Website Analytics",
+            "google_analytics",
+            {
+                "metric": "<metric>",
+                "start_date": "<start_date>",
+                "end_date": "<end_date>"
+            },
+            self.google_analytics
+        ),
+        return prompt
+
+    def can_handle_post_prompt(self) -> bool:
+        """This method is called to check that the plugin can
+        handle the post_prompt method.
+        Returns:
+            bool: True if the plugin can handle the post_prompt method."""
+        return True
+
     def can_handle_on_response(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_response method.
-
         Returns:
             bool: True if the plugin can handle the on_response method."""
         return False
 
-    @abc.abstractmethod
     def on_response(self, response: str, *args, **kwargs) -> str:
         """This method is called when a response is received from the model."""
         pass
 
-    @abc.abstractmethod
-    def can_handle_post_prompt(self) -> bool:
-        """This method is called to check that the plugin can
-        handle the post_prompt method.
-
-        Returns:
-            bool: True if the plugin can handle the post_prompt method."""
-        return False
-
-    @abc.abstractmethod
-    def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
-        """This method is called just after the generate_prompt is called,
-            but actually before the prompt is generated.
-
-        Args:
-            prompt (PromptGenerator): The prompt generator.
-
-        Returns:
-            PromptGenerator: The prompt generator.
-        """
-        pass
-
-    @abc.abstractmethod
     def can_handle_on_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_planning method.
-
         Returns:
             bool: True if the plugin can handle the on_planning method."""
         return False
 
-    @abc.abstractmethod
     def on_planning(
         self, prompt: PromptGenerator, messages: List[Message]
     ) -> Optional[str]:
         """This method is called before the planning chat completion is done.
-
         Args:
             prompt (PromptGenerator): The prompt generator.
             messages (List[str]): The list of messages.
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_planning method.
-
         Returns:
             bool: True if the plugin can handle the post_planning method."""
         return False
 
-    @abc.abstractmethod
     def post_planning(self, response: str) -> str:
         """This method is called after the planning chat completion is done.
-
         Args:
             response (str): The response.
-
         Returns:
             str: The resulting response.
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_pre_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_instruction method.
-
         Returns:
             bool: True if the plugin can handle the pre_instruction method."""
         return False
 
-    @abc.abstractmethod
     def pre_instruction(self, messages: List[Message]) -> List[Message]:
         """This method is called before the instruction chat is done.
-
         Args:
             messages (List[Message]): The list of context messages.
-
         Returns:
             List[Message]: The resulting list of messages.
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_on_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_instruction method.
-
         Returns:
             bool: True if the plugin can handle the on_instruction method."""
         return False
 
-    @abc.abstractmethod
     def on_instruction(self, messages: List[Message]) -> Optional[str]:
         """This method is called when the instruction chat is done.
-
         Args:
             messages (List[Message]): The list of context messages.
-
         Returns:
             Optional[str]: The resulting message.
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_instruction method.
-
         Returns:
             bool: True if the plugin can handle the post_instruction method."""
         return False
 
-    @abc.abstractmethod
     def post_instruction(self, response: str) -> str:
         """This method is called after the instruction chat is done.
-
         Args:
             response (str): The response.
-
         Returns:
             str: The resulting response.
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_pre_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_command method.
-
         Returns:
             bool: True if the plugin can handle the pre_command method."""
         return False
 
-    @abc.abstractmethod
     def pre_command(
         self, command_name: str, arguments: Dict[str, Any]
     ) -> Tuple[str, Dict[str, Any]]:
         """This method is called before the command is executed.
-
         Args:
             command_name (str): The command name.
             arguments (Dict[str, Any]): The arguments.
-
         Returns:
             Tuple[str, Dict[str, Any]]: The command name and the arguments.
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_command method.
-
         Returns:
             bool: True if the plugin can handle the post_command method."""
         return False
 
-    @abc.abstractmethod
     def post_command(self, command_name: str, response: str) -> str:
         """This method is called after the command is executed.
-
         Args:
             command_name (str): The command name.
             response (str): The response.
-
         Returns:
             str: The resulting response.
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_chat_completion(
         self, messages: Dict[Any, Any], model: str, temperature: float, max_tokens: int
     ) -> bool:
         """This method is called to check that the plugin can
           handle the chat_completion method.
-
         Args:
             messages (List[Message]): The messages.
             model (str): The model name.
             temperature (float): The temperature.
             max_tokens (int): The max tokens.
-
           Returns:
               bool: True if the plugin can handle the chat_completion method."""
         return False
 
-    @abc.abstractmethod
     def handle_chat_completion(
         self, messages: List[Message], model: str, temperature: float, max_tokens: int
     ) -> str:
         """This method is called when the chat completion is done.
-
         Args:
             messages (List[Message]): The messages.
             model (str): The model name.
             temperature (float): The temperature.
             max_tokens (int): The max tokens.
-
         Returns:
             str: The resulting response.
         """
         pass
+
+    def google_analytics(self, metric: str, start_date: str, end_date: str) -> Union[int, str]:
+        metric = f'ga:{metric}'
+        # Build the request body
+        request_body = {
+            'view_id': view_id,  # Replace with your Google Analytics view ID
+            'start_date': start_date,
+            'end_date': end_date,
+            'dimensions': {
+                'ga:sourceMedium',
+                'ga:date'
+            },
+            'metrics': {
+                metric
+            }
+        }
+
+        try:
+            # Get the GA data
+            response = c.get_all_data(request_body)
+            data = response['data']
+
+            # Aggregate the metric values
+            total = data['data'][metric].astype(int).sum()
+            return total
+        except KeyError:
+            return f'{metric} is not a valid metric.'
+        except Exception as e:
+            return f'An error occurred: {str(e)}'
+
+    def get_metrics_from_file(self):
+        try:
+            with open("metrics.txt", 'r') as file:
+                metrics = file.readlines()
+            return [metric.strip() for metric in metrics]
+        except FileNotFoundError:
+            print("Metrics file not found.")
+            return []
+
+    def metric_exists(self, metric: str) -> bool:
+        try:
+            metrics = self.get_metrics_from_file()
+            if metric in metrics:
+                return f"{metric} exists."
+            else:
+                return f"{metric} does not exist."
+        except Exception as e:
+            print(f"Error checking metric: {str(e)}")
+            return f"Error checking metric: {str(e)}"
